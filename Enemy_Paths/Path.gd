a086daf2ea63572cd96bd842160195d7f6e1c5dc
Mikko <mikko.ryynanen2@gmail.com>
2022-10-25 09:17:09 +0300

    Fixed enemy group spawning yield error

--- Enemy_Paths/Path.gd
+++ Enemy_Paths/Path.gd
@@ -1,15 +1,21 @@
 extends Path2D
 
 var speed = 25
+var can_move = false
 
 onready var path_follow = $PathFollow
 
 func _ready():
 	path_follow.offset = 0
 
+func start():
+	can_move = true
+
+
 func _process(delta):
-	if path_follow.unit_offset >= 1:
-		queue_free()
-		
-	else:
-		path_follow.offset += speed * delta
+	if can_move:
+		if path_follow.unit_offset >= 1:
+			queue_free()
+			
+		else:
+			path_follow.offset += speed * delta
--- Entities/Enemy/EnemyGroup.gd
+++ Entities/Enemy/EnemyGroup.gd
@@ -3,6 +3,7 @@ extends Node2D
 class_name EnemyGroup
 
 var enemies_count = 0
+var spawned_paths = []
 
 
 func _ready():
@@ -14,18 +15,19 @@ func init(count: int, path_index: int):
 
 	var EnemyPath = load(str("res://Enemy_Paths/Level_0/Path_", path_index, ".tscn"))
 	for i in count:
-		# TODO Having this yield here removes bug with class instance being gone after yield
-		# But this also adds start delay to spawning
-		yield(get_tree().create_timer(1.5), "timeout")
-
+		
 		var enemy_path = EnemyPath.instance()
 		self.add_child(enemy_path)
-
+		spawned_paths.append(enemy_path)
+		
 		var Enemy = load("res://Entities/Enemy/Enemy.tscn")
 		var enemy = Enemy.instance()
 		enemy_path.get_child(0).add_child(enemy)
-		# enemy.global_position = Vector2(-50, 20 * i)
-		# enemy.init(Vector2.RIGHT, self)
+		
+	for path in spawned_paths:
+		path.start()
+
+		yield(get_tree().create_timer(1.5), "timeout")
 
 
 func enemy_destroyed(entity):
