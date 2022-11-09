863375051e86881db6068fe774748b488ca616fc
Mikko <mikko.ryynanen2@gmail.com>
2022-11-09 16:20:01 +0200

    Fix PlayerStats stat calcualtion

--- Data/PlayerStats.gd
+++ Data/PlayerStats.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 var health: int = 75
 var movement: int = 0
-var damage: int = 5
+var damage: float = 5
 var energy: int = 0
 var fire_rate: float = 0.75
 
@@ -31,10 +31,10 @@ func load_stats():
 		var stat_for_level = DataLoader.get_stat_level(stat.ID, stat.Level)
 		if stat.Name == "Damage":
 			print("Adding damage by ", stat_for_level.Value)
-			damage += int(stat_for_level.Value)
+			damage = float(stat_for_level.Value)
 		elif stat.Name == "Movement":
 			print("Adding Movement by ", stat_for_level.Value)
-			movement += int(stat_for_level.Value)
+			movement = int(stat_for_level.Value)
 		elif stat.Name == "FireRate":
 			print("Setting FireRate to ", stat_for_level.Value)
 			fire_rate = float(stat_for_level.Value)
